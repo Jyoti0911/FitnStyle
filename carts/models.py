81f3e9722feef4d8a7e27ccd450d746c281e271c
Jyoti kumari <jyotikumari091199@gmail.com>
2025-11-10 16:01:10 +0530

    handled cart and checkout page for logged in users

--- carts/models.py
+++ carts/models.py
@@ -1,6 +1,7 @@
 
 from django.db import models
 from store.models import Product, Variation
+from accounts.models import Account
 
 
 
@@ -14,15 +15,16 @@ class Cart(models.Model):
         return self.cart_id
     
 class CartItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)  
-    variations = models.ManyToManyField(Variation, blank=True) 
-    cart= models.ForeignKey(Cart, on_delete=models.CASCADE)
-    quantity=models.IntegerField()
-    is_active= models.BooleanField(default=True)
-
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
+    variations = models.ManyToManyField(Variation, blank=True)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
 
     def sub_total(self):
         return self.product.price * self.quantity
+
 
 
     def __unicode__(self):
